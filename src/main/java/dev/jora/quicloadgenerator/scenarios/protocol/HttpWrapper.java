c78b39ee44069df0eafe5f7e928f3060ac567c16
dyadyajora <ja_massa97@mail.ru>
2021-12-05 19:25:40 +0300

    support disable certificate verification for https

--- src/main/java/dev/jora/quicloadgenerator/scenarios/protocol/HttpWrapper.java
+++ src/main/java/dev/jora/quicloadgenerator/scenarios/protocol/HttpWrapper.java
@@ -2,6 +2,10 @@ package dev.jora.quicloadgenerator.scenarios.protocol;
 
 import dev.jora.quicloadgenerator.scenarios.generator.BaseScenario;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.net.http.HttpClient;
 
 public class HttpWrapper extends BaseProtocolWrapper {
@@ -11,6 +15,34 @@ public class HttpWrapper extends BaseProtocolWrapper {
 
     @Override
     protected HttpClient buildHttpClient() {
-        return HttpClient.newHttpClient();
+        HttpClient.Builder builder = HttpClient.newBuilder();
+        if (this.scenario.getOptions().isDisableCertificateVerification()) {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+
+                        public void checkClientTrusted(
+                                java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+
+                        public void checkServerTrusted(
+                                java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sc;
+            try {
+                sc = SSLContext.getInstance("SSL");
+                sc.init(null, trustAllCerts, new java.security.SecureRandom());
+                HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            } catch (Exception e) {
+                return null;
+            }
+            builder.sslContext(sc);
+        }
+        return builder.build();
     }
 }
